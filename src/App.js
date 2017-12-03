1bdb5a4da5bc2192839cb1b9b79d4e4bf7637eec
Felipe Domingues <felipe.domingues@iMac-de-Felipe.local>
2017-12-03 14:11:32 -0200

    install some libraries
    react-redux
    react-route

--- src/App.js
+++ src/App.js
@@ -1,21 +1,58 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import {withRouter} from 'react-router-dom';
+import {connect} from 'react-redux';
+import PropTypes from 'prop-types';
+
+import {getCategories} from '../src/actions';
+
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+
+    static propTypes = {
+        categories: PropTypes.array
+    };
+
+    componentDidMount() {
+        this.props.getCategories();
+    }
+
+    render() {
+        const {categories} = this.props;
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <h1 className="App-title">Leitura-Udacity</h1>
+                </header>
+                <div className="allcategories">
+                    <div>
+                        <h6>Categories</h6>
+                        {categories &&
+                        categories.map(category => (
+                            <ol>
+                                <li key={category.name}>
+                                    <button>{category.name}</button>
+                                </li>
+                            </ol>
+                        ))}
+                        {console.log(this.props)}
+                    </div>
+                </div>
+            </div>
+
+        );
+    }
 }
 
-export default App;
+function mapStateToProps({categories}) {
+    return {
+        categories:categories
+    };
+}
+
+export default withRouter(
+    connect(mapStateToProps, {
+        getCategories
+    })(App)
+);
+
